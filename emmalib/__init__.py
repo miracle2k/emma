miracle2k/emma

commit 7862c1be891603be5dbe7bbe5f438ffce239f4cd

Do not hide output to stdout by default.

emmalib/__init__.py
@@ -3342,7 +3342,7 @@ def usage():
 	
 	print """usage: emma [-h|--help] [-d|--debug] [-l output_log [-f|--flush]]
  -h|--help     show this help message
- -d|--debug    output debug information on stdout
+ -q|--quiet    do not output debug information on stdout
  -l|--log FILE append all output to a specified log file
  -f|--flush    flush {stdout,log} after each write
 """
@@ -3351,7 +3351,7 @@ def usage():
 def start(args):
 	global new_instance
 
-	debug_output = False
+	no_debug_output = False
 	log_file = None
 	log_flush = False
 
@@ -3362,8 +3362,8 @@ def start(args):
 			continue
 		if arg == "-h" or arg == "--help":
 			usage()
-		elif arg == "-d" or arg == "--debug":
-			debug_output = True
+		elif arg == "-q" or arg == "--quiet":
+			no_debug_output = True
 		elif arg == "-f" or arg == "--flush":
 			log_flush = True
 		elif arg == "-l" or arg == "--log":
@@ -3374,7 +3374,8 @@ def start(args):
 			usage()
 
 	# this singleton will be accessible as sys.stdout!
-	output_handler(debug_output, log_file, log_flush)
+	# TODO: Use logging module rather than this custom solution.
+	output_handler(not no_debug_output, log_file, log_flush)
 
 	e = Emma()
 

commit 1bce7c676758653233743714b44f9f2019424e15

Do not create pretty format test query.
The concept of debug mode is also broken since 7862c1be.

emmalib/__init__.py
@@ -3324,7 +3324,6 @@ class output_handler:
 			self.log_fp = file(log_file, "a+")
 		else:
 			self.log_fp = None
-		self.debug = print_stdout or log_file
 
 	def write(self, s):
 		if self.print_stdout:

emmalib/plugins/pretty_format/__init__.py
@@ -169,19 +169,6 @@ class pretty_format:
 		self.install_toolbar_item("query_toolbar", gtk.STOCK_INDENT, "pretty format query", self.on_pretty_format)
 		self.install_toolbar_item("query_toolbar", gtk.STOCK_UNINDENT, "compress query", self.on_compress)
 		q = self.emma.current_query
-		if sys.stdout.debug:
-			# check if we are running with debug output - enable example text
-			print "\n\n\n"
-			self.set_query_text(q, """# this is the pretty format test query. click the "pretty format" or "compress query" button in the query-toolbar.
-       \n      \t# comment before\n\n/* also before... */\n   \tselect date_format \n\t (\nnow(  \n"lalala"  ) , "%Y-%m-%d"  \n), ("%Y,((%m"), \', from ),here\', * from record_job 
-where some_field
-= 
-"a very interesting 'text'"
-order by job_id desc,
-\tvalid_from,\n\t\t\n\tmode,\n\tquery,\n\tpriority desc limit 150;
-select * from user;
-""")
-
 
 	def cleanup(self):
 		for item, toolbar in self.toolbar_items: